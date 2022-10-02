c22b81917ff125d3c7879180eceb1de956f8f53e
Nafi Aufa <95265659+nafiaufa@users.noreply.github.com>
2022-10-02 15:12:07 +0700

    commit cuy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("socket connected");
+});
+
+server.listen(3000);
